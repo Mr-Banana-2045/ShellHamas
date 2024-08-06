e02ed723df440c4f9d749d49e4c86e8ed33b5cd7
Moz-Banana <109140672+Mr-Banana-2045@users.noreply.github.com>
2024-08-06 10:46:34 +0330

    Update terminal_programs.js

--- terminal_programs.js
+++ terminal_programs.js
@@ -1,4 +1,3 @@
-
 var PROGAMS = {
 
   help: function(...a) {
@@ -6,34 +5,65 @@ var PROGAMS = {
       "headers": ["command", "description"],
       "upload": ["file name code"],
       "clear": ["clear terminal"],
-      "whoami": ["you"]
+      "whoami": ["you"],
+      "ls": ["show direction"],
+      "cd": ["shell path"],
+      "delete": ["file delete"]
     });
   },
 
   upload: function(...args) {
-        const filename = args[0];
-        const content = args[1];
-        
-        if (!filename || !content) {
-            this.printa("Usage: upload filename content");
-            return;
-        }
-        
-        const blob = new Blob([content], { type: 'text/plain' });
-        const url = URL.createObjectURL(blob);
-        
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        URL.revokeObjectURL(url);
+    const filename = args[0];
+    const content = args[1];
+    const formData = new FormData();
+    formData.append('filename', filename);
+    formData.append('content', content);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'up.php', true);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        this.printa('Success:', xhr.responseText);
+      } else {
+        console.error('Error:', xhr.statusText);
+        this.printa('Error:', xhr.statusText);
+     }
+    };
+
+   xhr.send(formData);
   },
   clear: function(...a) {
     this.clear_terminal();
   },
   whoami: function(...a){
     this.printa("Sarbaz Hamas");
+  },
+  ls: function(...a){
+  fetch('dir.php')
+        .then(response => response.text())
+        .then(data => {
+            this.printa(data);
+        })
+        .catch(error => console.error('Error:', error));
+
+  },
+  delete: function(...args){
+  const file = args[0];
+  const xhr = new XMLHttpRequest();
+            xhr.open('POST', 'del.php', true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    this.printa(xhr.responseText);
+                }
+            };
+            xhr.send('filename=' + encodeURIComponent(file));
+  },
+  cd: function(...a) {
+   fetch('cd.php')
+        .then(response => response.text())
+        .then(data => {
+            this.printa(data);
+        })
+        .catch(error => console.error('Error:', error));
   }
 };
