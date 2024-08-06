905b46a72578de4e428dccecbe8356bb1d48859f
Moz-Banana <109140672+Mr-Banana-2045@users.noreply.github.com>
2024-08-06 12:24:21 +0330

    Update dir.php

--- dir.php
+++ dir.php
@@ -1,7 +1,7 @@
 <?php
 $directoryPath = getcwd();
 if ($handle = opendir($directoryPath)) {
-    echo "Files and directories in '$directoryPath':\n";
+    echo $directoryPath':\n';
 
     while (false !== ($entry = readdir($handle))) {
         if ($entry != "." && $entry != "..") {
